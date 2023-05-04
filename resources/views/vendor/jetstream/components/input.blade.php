9fb4a9855e5f5c95795cf96bcc307bc937aa4f9a
Jamesson Cardozo <jamessoncardozo@gmail.com>
2023-05-04 14:05:21 +0000

    Jetstream Atualizado

--- resources/views/vendor/jetstream/components/input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
